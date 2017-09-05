f4c5c7a9f5d80f4ff689b56f3e1d3ac05703df7c
Mariatta <Mariatta@users.noreply.github.com>
2017-09-05 13:39:09 -0700

    Only backport and leave comment if backport labels are found. (#2)

--- backport/backport_pr.py
+++ backport/backport_pr.py
@@ -21,16 +21,19 @@ async def backport_pr(event, gh, *args, **kwargs):
                                              {'number': f"{event.data['pull_request']['number']}"})
 
         pr_labels = await gh.getitem(gh_issue['labels_url'])
-        branches = [label['name'].split()[-1] for label in pr_labels if label['name'].startswith("needs backport to")]
+        branches = [label['name'].split()[-1]
+                    for label in pr_labels
+                        if label['name'].startswith("needs backport to")]
 
-        message = "🐍🍒⛏🤖" \
-                  f"Thanks @{created_by} for the PR, and @{merged_by} for merging it 🌮🎉." \
-                  f"I'm working now to backport this PR to: {', '.join(branches)}."
-        util.comment_on_pr(issue_number, message)
+        if branches:
+            message = "🐍🍒⛏🤖 " \
+                      f"Thanks @{created_by} for the PR, and @{merged_by} for merging it 🌮🎉." \
+                      f"I'm working now to backport this PR to: {', '.join(branches)}."
+            util.comment_on_pr(issue_number, message)
 
-        for branch in branches:
-            tasks.backport_task.delay(commit_hash,
-                                      branch,
-                                      issue_number=issue_number,
-                                      created_by=created_by,
-                                      merged_by=merged_by)
+            for branch in branches:
+                tasks.backport_task.delay(commit_hash,
+                                          branch,
+                                          issue_number=issue_number,
+                                          created_by=created_by,
+                                          merged_by=merged_by)
