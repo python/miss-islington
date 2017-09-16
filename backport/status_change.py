c268165e44456e67d9d02f6c4f60e23699663514
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-09-16 08:03:39 -0700

    Fix data retrieval

--- backport/status_change.py
+++ backport/status_change.py
@@ -17,7 +17,7 @@ async def check_status(event, gh, *args, **kwargs):
     """
     Check the state change
     """
-    if event.data["commit"]["author"]["login"] == "miss-islington":
+    if event.data["author"]["login"] == "miss-islington":
         sha = event.data["sha"]
         status_url = f"https://api.github.com/repos/python/cpython/commits/{sha}/status"
         request_headers = sansio.create_headers(
