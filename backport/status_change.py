537f898645a1f6e96bf106874e747f712babc8a0
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-09-16 10:49:00 -0700

    print data

--- backport/status_change.py
+++ backport/status_change.py
@@ -17,6 +17,10 @@ async def check_status(event, gh, *args, **kwargs):
     """
     Check the state change
     """
+    print("event data")
+    print(event.data)
+    print("author")
+    print(event.data["commit"]["author"]["login"])
     if event.data["commit"]["author"]["login"] == "miss-islington":
         sha = event.data["sha"]
         status_url = f"https://api.github.com/repos/python/cpython/commits/{sha}/status"
