57a0b47ab3f6138104aee45319b8a6eefcd65563
Mariatta Wijaya <mariatta.wijaya@gmail.com>
2017-09-16 08:28:46 -0700

    debugging

--- backport/status_change.py
+++ backport/status_change.py
@@ -17,7 +17,8 @@ async def check_status(event, gh, *args, **kwargs):
     """
     Check the state change
     """
-    if event.data["author"]["login"] == "miss-islington":
+    print(event.data)
+    if event.data["commit"]["author"]["login"] == "miss-islington":
         sha = event.data["sha"]
         status_url = f"https://api.github.com/repos/python/cpython/commits/{sha}/status"
         request_headers = sansio.create_headers(
