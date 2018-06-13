82569cd67c961f882d9489eda257c42a972ad697
Mariatta Wijaya <mariatta@python.org>
2018-06-12 19:58:34 -0700

    Don't say thanks after PR has been closed.
    Save one API call.
    
    Closes https://github.com/python/miss-islington/issues/97

--- miss_islington/delete_branch.py
+++ miss_islington/delete_branch.py
@@ -1,7 +1,5 @@
 import gidgethub.routing
 
-from . import util
-
 router = gidgethub.routing.Router()
 
 
@@ -9,17 +7,8 @@ router = gidgethub.routing.Router()
 async def delete_branch(event, gh, *args, **kwargs):
     """
     Delete the branch once miss-islington's PR is closed.
-    Say thanks if it's merged.
     """
     if event.data["pull_request"]["user"]["login"] == "miss-islington":
-        if event.data["pull_request"]["merged"]:
-            issue_number = event.data['pull_request']['number']
-            merged_by = event.data['pull_request']['merged_by']['login']
-            if merged_by != "miss-islington":
-                await util.leave_comment(gh, issue_number, f"Thanks, @{merged_by}!")
-            else:
-                await util.leave_comment(gh, issue_number, "Thanks!")
-
         branch_name = event.data['pull_request']['head']['ref']
         url = f"/repos/miss-islington/cpython/git/refs/heads/{branch_name}"
         await gh.delete(url)
