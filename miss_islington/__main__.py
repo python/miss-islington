590d228255dacdbf2ad796b2050d1bc3eae7b4a2
Łukasz Langa <lukasz@langa.pl>
2021-08-02 14:38:35 +0200

    Setup Sentry for aiohttp as well

--- miss_islington/__main__.py
+++ miss_islington/__main__.py
@@ -8,6 +8,9 @@ import cachetools
 from aiohttp import web
 from gidgethub import aiohttp as gh_aiohttp
 from gidgethub import routing, sansio
+import sentry_sdk
+from sentry_sdk.integrations.aiohttp import AioHttpIntegration
+
 
 from . import backport_pr, check_run, delete_branch, status_change
 
@@ -53,11 +56,11 @@ GH delivery ID {event.delivery_id} \
         return web.Response(status=500)
 
 
-if __name__ == "__main__":  # pragma: no cover
-    app = web.Application()
-    app.router.add_post("/", main)
-    port = os.environ.get("PORT")
-    if port is not None:
-        port = int(port)
+sentry_sdk.init(dsn=os.environ.get("SENTRY_DSN"), integrations=[AioHttpIntegration()])
+app = web.Application()
+app.router.add_post("/", main)
+port = os.environ.get("PORT")
+if port is not None:
+    port = int(port)
 
-    web.run_app(app, port=port)
+web.run_app(app, port=port)
