b19c203784c5f6e6f91e15359f95af2164404ab1
Łukasz Langa <lukasz@langa.pl>
2022-09-09 13:54:20 +0200

    Disable CI status change comments unless the "automerge" label is used
    Works around #577

--- miss_islington/status_change.py
+++ miss_islington/status_change.py
@@ -22,7 +22,8 @@ async def check_status(event, gh, *args, **kwargs):
         event.data["commit"].get("committer")
         and event.data["commit"]["committer"]["login"] == "miss-islington"
     ):
-        await check_ci_status_and_approval(gh, sha, leave_comment=True)
+        # Leave comment temporarily disabled when automerge not used. See #577.
+        await check_ci_status_and_approval(gh, sha, leave_comment=False)
     else:
         pr_for_commit = await util.get_pr_for_commit(gh, sha)
         if pr_for_commit:
