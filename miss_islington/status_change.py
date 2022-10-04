cce2f8f0eae4955f8d4adbe34195602feef49a0a
Łukasz Langa <lukasz@langa.pl>
2022-10-05 00:27:37 +0200

    Reformat a little

--- miss_islington/status_change.py
+++ miss_islington/status_change.py
@@ -116,14 +116,13 @@ async def check_ci_status_and_approval(
                     message = f"Status check is done, and {status} {emoji}."
                     if not success:
                         # if is_automerge:
-                            participants = await util.get_gh_participants(gh, pr_number)
-                            message = f"{participants}: {message}"
+                        participants = await util.get_gh_participants(gh, pr_number)
                         # else:
                         #    original_pr_number = title_match.group("pr")
                         #    participants = await util.get_gh_participants(
                         #        gh, original_pr_number
                         #    )
-                        #    message = f"{participants}: {message}"
+                        message = f"{participants}: {message}"
                     await util.leave_comment(
                         gh,
                         pr_number=pr_number,
